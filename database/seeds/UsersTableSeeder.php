9a8e1ff3e50f92bd59008aaf425b92e4a5d53408
yyh-gl <yhonda.95.gl@gmail.com>
2018-08-29 02:52:39 +0900

    コメント追加

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
             'email' => 'test@mikilab.doshisha.ac.jp',
             'avatar' => 'hogehoge',
             'introduction' => 'はじめまして',
+            // パスワード：secret
         ]);
     }
 }
