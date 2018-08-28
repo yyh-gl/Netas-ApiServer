76073f459cbc096941919226f7ac4f392032285b
yyh-gl <yhonda.95.gl@gmail.com>
2018-08-29 02:50:13 +0900

    全てのAPIの認証機能をON

--- routes/api.php
+++ routes/api.php
@@ -1,35 +1,29 @@
 <?php
 
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/users', function (Request $request) {
-    return $request->user();
-});
-
-/*
-|--------------------------------------------------------------------------
-| User Routes
+| API Version 1 Routes
 |--------------------------------------------------------------------------
 */
 Route::namespace('Api\V1')->group(function () {
-    Route::group(['prefix' => 'v1'], function () {
+    Route::group(['middleware' => 'auth:api', 'prefix' => 'v1'], function () {
+
+        /*
+        |------------------------------------------------------------------
+        | User Routes
+        |------------------------------------------------------------------
+        */
         Route::group(['prefix' => 'users'], function () {
-            Route::get('/', 'UserController@index');
+            Route::middleware('auth:api')->get('/', 'UserController@index');
             Route::get('/{user_id}', 'UserController@show');
             Route::post('/', 'UserController@store');
         });
-
+        
+        /*
+        |------------------------------------------------------------------
+        | Tag Routes
+        |------------------------------------------------------------------
+        */
         Route::group(['prefix' => 'tags'], function () {
             Route::get('/', 'TagController@index');
             Route::get('/{id}', 'TagController@show');
